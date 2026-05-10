9834d61e33b586d455d75d0bcdea7b602d35b006
sheikha200 <sheikhm0011@gmail.com>
2026-05-10 12:47:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -149,9 +149,6 @@ def save_note():
 
     flash("Note Saved Securely!")
     return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-
-    app.run(debug=True)
+    
+with app.app_context():
+    db.create_all()
